4f4443dc2fa2e9e321f68845be8b4083ce3f150e
NiksinGEG <91534986+NiksinGEG@users.noreply.github.com>
2022-03-20 22:25:14 +0300

    Примитивная хуйня на доработку

---
+++ Assets/Map/WorldMap/HexGrid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Map.WorldMap
+{
+    public class HexGrid : MonoBehaviour
+    {
+        public int width = 6;
+        public int height = 6;
+
+        public HexCell cell_prefab;
+
+		HexCell[] cells;
+		HexMesh hexMesh;
+
+		[ContextMenu("Generate game field")]
+		void Awake()
+		{
+			hexMesh = GetComponentInChildren<HexMesh>();
+			cells = new HexCell[height * width];
+			for (int z = 0, i = 0; z < height; z++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					CreateCell(x, z, i++);
+				}
+			}
+		}
+
+		void Start()
+		{
+			hexMesh.Triangulate(cells);
+		}
+
+		void CreateCell(int x, int z, int i)
+		{
+			Vector3 position;
+			position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+			position.y = 0f;
+			position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+			HexCell cell = cells[i] = Instantiate<HexCell>(cell_prefab);
+			cell.transform.SetParent(transform, false);
+			cell.transform.localPosition = position;
+		}
+	}
+}
---
+++ Assets/Map/WorldMap/HexMetrics.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMetrics : MonoBehaviour
+{
+    public const float outerRadius = 10f;   //Внешний радиус шестиугольника
+    public const float innerRadius = outerRadius * 0.866025404f;    //Внутренний радиус шестиугольника
+
+	//Эта хуйня составная, поэтому забиндим положение фигур внутри
+	//Возможные ориентации внутри шестиугольника
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(0f, 0f, outerRadius)
+	};
+
+
+}
