aeb0948af7a3e1060e6e61f9c098634442f8652c
NiksinGEG <91534986+NiksinGEG@users.noreply.github.com>
2022-03-31 21:42:10 +0300

    Revert "123"
    This reverts commit 0122b25708860357765e9ef24d7990f3fb88a4c8.

--- Assets/Map/MainMap.cs
+++ Assets/Map/MainMap.cs
@@ -6,22 +6,5 @@ using Assets.Map.MapResources;
 
 public class MainMap : MonoBehaviour
 {
-    public HexGrid grid;
-
-    public MapResource rockPrefab;
-
-    MapResource[] resources;
-    List<MapResource> additionFeatures;
-
-    private void Awake()
-    {
-        grid = Instantiate(grid);
-        grid.transform.SetParent(transform);
-        additionFeatures = new List<MapResource>();
-        resources = new MapResource[HexMetrics.chunkSizeX * grid.chunkCountX * HexMetrics.chunkSizeZ * grid.chunkCountZ];
-        System.Random rndSeed = new System.Random(300);
-
-        ResourceGenerator resGen = new ResourceGenerator();
-        resGen.GenerateResource(grid, resources, additionFeatures, rndSeed);
-    }
+   
 }
---
+++ Assets/Map/PrefabManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabManager : MonoBehaviour
+{
+    public MapResource RockPrefab;
+    public MapResource TreePrefab;
+    //public .....
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+}
