e8f9e5222968eebede03501b4aa031e7d7254afd
NiksinGEG <91534986+NiksinGEG@users.noreply.github.com>
2022-04-25 16:36:48 +0300

    Требуется фикс Безье

--- Assets/Scripts/Systems/MoveSystem.cs
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -59,21 +59,15 @@ public class MoveSystem : IECSSystem
                     fromRotation = c.gameObject.transform.rotation;
                     toRotation = Quaternion.LookRotation(point - c.gameObject.transform.position);
 
-                    Debug.Log($"Angle: {Quaternion.Angle(fromRotation, toRotation)}");
-
                     if (Quaternion.Angle(fromRotation, toRotation) < 0.00001f)
                         isTurned = true;
                     else
                         isTurned = false;
 
                     if (isTurned)
-                    {
                         c.gameObject.transform.position = Vector3.MoveTowards(c.gameObject.transform.position, c.WayCells[0].transform.position, c.MoveSpeed);
-                    }
                     else
-                    {
-                        c.gameObject.transform.rotation = Quaternion.Slerp(fromRotation, toRotation, c.RotationSpeed);
-                    }
+                        c.gameObject.transform.rotation = Quaternion.RotateTowards(fromRotation, toRotation, c.RotationSpeed);
                 }
                 else
                 {
@@ -82,11 +76,23 @@ public class MoveSystem : IECSSystem
                     Vector3 middle = c.WayCells[0].transform.position;
 
                     point = Bezier.GetPoint(start, middle, end, c.t);
+                    point.y = c.gameObject.transform.position.y;
+                    fromRotation = c.gameObject.transform.rotation;
                     toRotation = Quaternion.LookRotation(point - c.gameObject.transform.position);
-
-                    c.gameObject.transform.rotation = toRotation;
-                    c.gameObject.transform.position = point;
-                    c.t += c.MoveSpeed * 0.05f;
+                    float angle = Quaternion.Angle(fromRotation, toRotation);
+                    Debug.Log($"Angle: {angle}");
+                    if (Quaternion.Angle(fromRotation, toRotation) < 20.0f && Quaternion.Angle(fromRotation, toRotation) > -1.0f)
+                        isTurned = true;
+                    else
+                        isTurned = false;
+                    if(isTurned)
+                    {
+                        c.gameObject.transform.rotation = toRotation;
+                        c.gameObject.transform.position = point;
+                        c.t += c.MoveSpeed * 0.05f;
+                    }
+                    else
+                        c.gameObject.transform.rotation = Quaternion.RotateTowards(fromRotation, toRotation, c.RotationSpeed);
                 }
             }
         }
